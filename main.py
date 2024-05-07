239c9fc55a8cdecb5955655d7009fa98ebadb07b
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2024-05-07 11:10:23 -0400

    further pre-process the image

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import cv2
-import tensorflow as tf
 
 image = cv2.imread('./data/test-image/test.png')
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+denoised_gray = cv2.bilateralFilter(gray, 9, 75, 75)
+
 face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 
-faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+faces = face_cascade.detectMultiScale(denoised_gray, scaleFactor=1.1, minNeighbors=5)
 
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)  # Blue rectangle
